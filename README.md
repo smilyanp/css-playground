ba38331eb415ca48074d34c75867426c8577e923
Smilyan Pavlov <smilyan.pavlov@gmail.com>
2023-08-23 18:51:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Test repo trying to work with CSS layouts and learn how to do them from scratch.
+
+We'll mainly be re-creating this design: https://horizon-ui.com/horizon-tailwind-react/admin/default
+
+Useful resources: https://github.com/smilyanp/react-components/tree/master/src/css
---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="sidebar">
+        <h2>HORIZON FREE</h2>
+        <hr />
+        <nav>
+          <ul>
+            <li>
+              <a href="">Main Dashboard</a>
+            </li>
+            <li>
+              <a href="">NFT Marketplace</a>
+            </li>
+            <li>
+              <a href="">Data Tables</a>
+            </li>
+            <li>
+              <a href="">Profile</a>
+            </li>
+            <li>
+              <a href="">Sign In</a>
+            </li>
+            <li>
+              <a href="">RTL Admin</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="main_content">
+        Content Content Content Content Content Content Content Content Content
+        Content Content Content Content Content{" "}
+      </div>
+    </div>
+  );
+}
+
+export default App;
